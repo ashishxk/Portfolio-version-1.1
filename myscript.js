6644a8ee1a18104bcde99ee44b9e129cc9c3e1b8
ashy31 <56349542+ashy31@users.noreply.github.com>
2022-05-21 01:40:43 +0530

    commit

---
+++ myscript.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "10px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+} 
+
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
